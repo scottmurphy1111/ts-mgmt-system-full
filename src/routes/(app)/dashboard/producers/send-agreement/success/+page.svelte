f03e1aeb926922f2a29374defe7469f87a3a4c73
Scott Murphy <scottmurphy1111@gmail.com>
2024-05-02 22:31:36 -0400

    send agreement statusCode, success page done

---
+++ src/routes/(app)/dashboard/producers/send-agreement/success/+page.svelte
@@ -0,0 +1,9 @@
+<div class="flex flex-col gap-8 p-8 w-full">
+	<h2 class="h2 mb-4">Success!</h2>
+	Your producer agreements were sent successfully!
+	<div class="inline-flex gap-4 w-auto">
+		<a class="btn-primary w-auto" href="/dashboard/producers/send-agreement">Send more agreements</a
+		>
+		<a class="btn-primary w-auto" href="/dashboard/producers">Back to producers</a>
+	</div>
+</div>
